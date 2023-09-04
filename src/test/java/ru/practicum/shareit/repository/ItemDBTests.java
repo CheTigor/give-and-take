49064b91dd543c033ffcb3221347c04ca580f2eb
CheTigor <chetvertakov.igor@yandex.ru>
2023-09-04 20:43:15 +0300

    fix: another fix ItemDBTests

--- src/test/java/ru/practicum/shareit/repository/ItemDBTests.java
+++ src/test/java/ru/practicum/shareit/repository/ItemDBTests.java
@@ -22,15 +22,15 @@ public class ItemDBTests {
 
     @Test
     void searchItemByQuery() {
-        User user = new User(1L, "name", "email@mail.ru");
-        Item item1 = new Item(null, "name", "description", true, user, null);
-        Item item2 = new Item(null, "name123", "description", true, user, null);
-        Item item3 = new Item(null, "name", "description123", false, user, null);
-        userRepository.save(user);
-        repository.save(item1);
-        repository.save(item2);
-        repository.save(item3);
-        item2.setId(2L);
+        final User userNoId1 = new User(null, "name", "email@mail.ru");
+        final User user1 = userRepository.save(userNoId1);
+        final Item itemNoId1 = new Item(null, "name", "description", true, user1, null);
+        final Item itemNoId2 = new Item(null, "name123", "description", true, user1, null);
+        final Item itemNoId3 = new Item(null, "name", "description123", false, user1, null);
+        final Item item1 = repository.save(itemNoId1);
+        final Item item2 = repository.save(itemNoId2);
+        final Item item3 = repository.save(itemNoId3);
+        itemNoId2.setId(2L);
         List<Item> items = repository.findByQueryIgnoreCase("123", PageRequest.of(0, 20)).toList();
         Assertions.assertEquals(items, List.of(item2));
     }
